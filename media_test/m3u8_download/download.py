68d86b1d654530ae4edc3e7a8a3949bafbfced11
chenzuoli <chenzuoli709@163.com>
2024-08-03 22:52:06 +0800

    download一个m3u8视频

--- media_test/m3u8_download/download.py
+++ media_test/m3u8_download/download.py
@@ -7,7 +7,8 @@ import m3u8_To_MP4
 if __name__ == '__main__':
     # 1. Download videos from uri.
     # m3u8_To_MP4.multithread_download('https://svipsvip.ffzyread1.com/20240424/26411_4538b0e5/index.m3u8')
-    m3u8_To_MP4.multithread_download('https://v5.tlkqc.com/wjv5/202405/04/LsJM6rsQ0p77/video/index.m3u8')
+    # m3u8_To_MP4.multithread_download('https://v5.tlkqc.com/wjv5/202405/04/LsJM6rsQ0p77/video/index.m3u8')
+    m3u8_To_MP4.multithread_download('https://v11.tlkqc.com/wjv11/202407/14/HbwxbpbTnF83/video/index.m3u8')
 
     # 2. Download videos from existing m3u8 files.
     # m3u8_file_path = '/Users/zuolichen/Downloads/index.m3u8'
